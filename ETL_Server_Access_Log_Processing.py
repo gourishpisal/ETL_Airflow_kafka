81790ecba2cf3a969949b5a4d8279e3bc1749017
Gourish Pisal <62263679+gourishpisal@users.noreply.github.com>
2023-08-11 02:23:57 -0400

    Extract_transform_dags

---
+++ ETL_Server_Access_Log_Processing.py
@@ -0,0 +1,55 @@
+# import the libraries
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to write tasks!
+from airflow.operators.bash_operator import BashOperator
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'Gourish Pisal',
+    'start_date': days_ago(0),
+    'email': ['pisalgourish@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'ETL_Server_Access_Log_Processing',
+    default_args=default_args,
+    description='My first DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+# define the tasks
+# define the task 'download'
+download = BashOperator(
+    task_id='download',
+    bash_command='wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Apache%20Airflow/Build%20a%20DAG%20using%20Airflow/web-server-access-log.txt"',
+    dag=dag,
+)
+
+extract = BashOperator(
+    task_id='extract',
+    bash_command='cut -f1,4 -d"#" web-server-access-log.txt > /home/project/airflow/dags/extracted.txt',
+    dag=dag,
+)
+#The transform task must capitalize the visitorid.
+transform = BashOperator(
+    task_id='transform',
+    bash_command='tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/extracted.txt > /home/project/airflow/dags/capitalized.txt',
+    dag=dag,
+)
+
+#The load task must compress the extracted and transformed data.
+load = BashOperator(
+    task_id='load',
+    bash_command='zip log.zip capitalized.txt' ,
+    dag=dag,
+)
+
+# task pipeline
+download >> extract >> transform >> load
